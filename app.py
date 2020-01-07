e7510bb23bd11f01660398256eb579b60794bf72
Shankar <123456789987654321sr@gmail.com>
2020-01-07 16:06:30 +0530

    added procfile

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return "<h1> Home Page Of shankar App </h1>"
-
+@app.route('/api')
+def api():
+    return "API PAGE"
 if __name__=='__main__':
     app.run()
